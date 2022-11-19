f592677f2ccf2a660fd27597c96a18ccf3ad4079
unknown <angelaescariocorpuz@gmail.com>
2022-11-19 13:31:48 +0800

    added module for text-to-speech and made it possible for the virtual assistant to speak

--- virtual assistant.py
+++ virtual assistant.py
@@ -1,8 +1,17 @@
 # Youtube link used for this program: https://youtu.be/AWvsXxDtEkU (creating a virtual assistant by Programming Hero)
 
 import speech_recognition as sr
+import pyttsx3
 
 listener = sr.Recognizer ()
+engine = pyttsx3.init ()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say (text)
+    engine.runAndWait ()
+
 try:
     with sr.Microphone () as source:
         print ('Listening...')
@@ -10,6 +19,6 @@ try:
         command = listener.recognize_google(voice)
         command = command.lower()
         if 'alexa' in command:
-            print (command)
+            talk (command)
 except:
     pass
