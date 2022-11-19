eb27bceafd346c035840d6e369352441412affb8
unknown <angelaescariocorpuz@gmail.com>
2022-11-19 13:23:24 +0800

    added module where the program can recognize your voice and added code where the program can listen to what you're saying and print it out

--- virtual assistant.py
+++ virtual assistant.py
@@ -1,1 +1,12 @@
 # Youtube link used for this program: https://youtu.be/AWvsXxDtEkU (creating a virtual assistant by Programming Hero)
+
+import speech_recognition as sr
+
+listener = sr.Recognizer ()
+try:
+    with sr.Microphone () as source:
+        print ('Listening...')
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+except:
+    pass
