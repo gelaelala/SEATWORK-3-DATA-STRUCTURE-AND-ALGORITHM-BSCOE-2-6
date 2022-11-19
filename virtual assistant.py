11400383d45e9c20cacf13850a2aa6bf97aa905a
unknown <angelaescariocorpuz@gmail.com>
2022-11-19 14:43:14 +0800

    added comments

--- virtual assistant.py
+++ virtual assistant.py
@@ -18,15 +18,15 @@ def talk(text):
 
 def take_command ():
     try:
-        with sr.Microphone () as source:
+        with sr.Microphone () as source: # using microphone as the source for the user's command
             listen = "Listening..."
             print (listen)
             talk (listen)
             print ()
-            voice = listener.listen(source)
+            voice = listener.listen(source) # the program will listen to what was the user's saying
             command = listener.recognize_google(voice)
             command = command.lower()
-            if 'alexa' in command:
+            if 'alexa' in command: # the program will only continue if they hear the word "alexa" during the user's command
                 command = command.replace ('alexa', '')
                 print (command)
                 print ()
@@ -36,51 +36,51 @@ def take_command ():
 
 def run_alexa ():
     command = take_command ()
-    if 'play' in command:
+    if 'play' in command: # the virtual assistant will play the song in Youtube with the help of pywhatkit
         song = command.replace ('play', '')
         song_play = 'Playing' + song
         print (song_play)
         print()
         talk (song_play)
         pywhatkit.playonyt (song)
-    elif 'time' in command:
+    elif 'time' in command: # the virtual assistant will tell the time (format of time will be in hours:minutes and is stated in 12-hour format instead of 24-hour format (e.g. 12:22 PM))
         time = datetime.datetime.now().strftime('%I:%M %p')
         time_today = "The current time is " + time
         print (time_today)
         print()
         talk (time_today)
-    elif 'date' in command:
+    elif 'date' in command: # the virtual assistant will tell the date today (format of date will be in "month" "day", "year" (e.g. November 19, 2022))
         date = datetime.datetime.now().strftime ('%B %d, %Y')
         date_today = "The date today is " + date
         print (date_today)
         print()
         talk (date_today)
-    elif 'search for' in command:
+    elif 'search for' in command: # the virtual assistant will search for the subject in wikipedia and will give a 2-setence summary
         search = command.replace ('search for', '')
         wiki_info = wikipedia.summary(search, 2)
         print (wiki_info)
         print()
         talk (wiki_info)
-    elif 'how are you' in command:
+    elif 'how are you' in command: # the virtual assistant will reply to the user if they heard the "how are you" phrase
         great = "I'm doing great today."
         print (great)
         print()
         talk (great)
-    elif 'free today' in command:
+    elif 'free today' in command: # the virtual assistant will reply to the user if they heard the "free today" phrase
         free = "Not really. I still have a lot to do today."
         print (free)
         print()
         talk (free)
-    elif 'joke' in command:
+    elif 'joke' in command: # the virtual assistant will tell the user a joke with the help of pyjokes module
         joke = pyjokes.get_joke()
         print (joke)
         print()
         talk (joke)
-    else: 
+    else: # if the user didnt say anything or didnt hear any of the keywords above
         say = "I didn't understand that, please say it again."
         print (say)
         print ()
         talk (say)
 
-while True:
+while True: # program will loop after each conversation with the virtual assistant
     run_alexa ()
